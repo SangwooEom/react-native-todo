b413316007d03424b32bd1f7946209407bfb6674
sangwooeom <cdm19720@gamil.com>
2023-04-22 01:34:48 +0900

    타이틀 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import styled, {ThemeProvider} from 'styled-components/native';
 import {theme, Theme} from './theme';
+import {StatusBar} from 'react-native';
 
-const Container = styled.View<{theme: Theme}>`
+const Container = styled.SafeAreaView<{theme: Theme}>`
   flex: 1;
   background-color: ${props => props.theme.background};
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text<{theme: Theme}>`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${props => props.theme.main};
+  align-self: flex-start;
+  margin: 0px 20px;
 `;
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container />
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.background}
+        />
+        <Title>TODO List</Title>
+      </Container>
     </ThemeProvider>
   );
 }
